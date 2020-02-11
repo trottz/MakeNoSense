496f5ffa64f63bd6e4030dcae55427674ad86cc1
Chanon Cserepy <novachanon@gmail.com>
2020-02-11 16:10:02 -0600

    extend B and modify Driver to show it

---
+++ A.java
@@ -0,0 +1,9 @@
+public class A extends B{
+    private int data;
+
+    public A(int data){
+        //super(data);
+        this.data = data;
+    }
+
+}
--- B.java
+++ B.java
@@ -2,6 +2,10 @@ public class B{
 
         protected int data;
         
+
+        public B(){
+            this(0);
+        }
         public B(int data){
             this.data = data;
         }
