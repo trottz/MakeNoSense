df58b2017a27db453ff3f10191eef34bcc7acb89
Chanon Cserepy <novachanon@gmail.com>
2020-02-11 15:40:22 -0600

    Add an action similar to ignmoring professors

---
+++ B.java
@@ -0,0 +1,14 @@
+public class B{
+
+        protected int data;
+        
+        public B(int data){
+            this.data = data;
+        }
+
+        @Override
+        public String toString(){
+            return new String ("Value of data in V is " + 
+                                Integer.toString(data));
+        }
+}
